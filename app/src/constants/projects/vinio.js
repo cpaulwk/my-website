5671eb835c374a78ed7e58180da7c3506157d6ba
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:44:03 +0100

    create constants for vinio project

---
+++ app/src/constants/projects/vinio.js
@@ -0,0 +1,102 @@
+const hero = [
+  {
+    image: "",
+    title: "Vinio",
+    description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes:",
+    technologies: [
+      {
+        name: "MySQL",
+        icon: "mysql",
+      },
+      {
+        name: "SQL",
+        icon: "sql",
+      },
+      {
+        name: "NodeJS",
+        icon: "nodejs",
+      },
+      {
+        name: "ExpressJS",
+        icon: "expressjs",
+      },
+      {
+        name: "JavaScript",
+        icon: "javascript",
+      },
+      {
+        name: "TypeScript",
+        icon: "typescript",
+      },
+      {
+        name: "React",
+        icon: "react",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "tailwindcss",
+      },
+      {
+        name: "NextJS",
+        icon: "nextjs",
+      },
+      {
+        name: "Vercel",
+        icon: "vercel",
+      },
+    ],
+  }
+];
+
+const faq = [
+  {
+    title: "My thoughts & process",
+    thoughts: [
+      {
+        question: "Why did I start this project?",
+        answer: "[placeholder]",
+      },
+      {
+        question: "What do I want to achieve?",
+        answer: "[placeholder]",
+      },
+      {
+        question: "What are the main difficulties I faced?",
+        answer: "[placeholder]",
+      },
+      {
+        question: "What did I learn?",
+        answer: "[placeholder]",
+      },
+    ]
+  }
+];
+
+const explanations = [
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+];
+
+const cta = [
+  {
+    title: "Visit the web application",
+    description: "[description]",
+    repository: "https://github.com/stars/cpaulwk/lists/vinio",
+    deployment: "https://vinio-frontend.vercel.app",
+  }
+];
+
+export { hero, faq, explanations }
