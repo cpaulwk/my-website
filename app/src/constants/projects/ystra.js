217b3e34ea62f66b4f4ac7d0bc7cad4bcc4cbcb2
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:43:51 +0100

    create constants for ystra project

---
+++ app/src/constants/projects/ystra.js
@@ -0,0 +1,90 @@
+const hero = [
+  {
+    image: "",
+    title: "Ystra",
+    description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes",
+    technologies: [
+      {
+        name: "MongoDB",
+        icon: "mongodb",
+      },
+      {
+        name: "NodeJS",
+        icon: "nodejs",
+      },
+      {
+        name: "ExpressJS",
+        icon: "expressjs",
+      },
+      {
+        name: "JavaScript",
+        icon: "javascript",
+      },
+      {
+        name: "React Native",
+        icon: "reactnative",
+      },
+      {
+        name: "Expo Go",
+        icon: "expogo",
+      },
+      {
+        name: "Redux",
+        icon: "redux",
+      },
+    ],
+  }
+];
+
+const faq = [
+  {
+    title: "My thoughts & process",
+    thoughts: [
+      {
+        question: "Why did I start this project?",
+        answer: "[answer]",
+      },
+      {
+        question: "What do I want to achieve?",
+        answer: "[answer]",
+      },
+      {
+        question: "What are the main difficulties I faced?",
+        answer: "[answer]",
+      },
+      {
+        question: "What did I learn?",
+        answer: "[answer]",
+      },
+    ]
+  }
+];
+
+const explanations = [
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+  {
+    image: "[image]",
+    title: "[title]",
+    description: "[description]",
+  },
+];
+
+const cta = [
+  {
+    title: "Visit the web application",
+    description: "[description]",
+    repository: "https://github.com/stars/cpaulwk/lists/ystra",
+    deployment: "",
+  }
+];
+
+export { hero, faq, explanations, cta }
