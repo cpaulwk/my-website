fbec67442507be1a2ac19a3d589b18e6ac21c68d
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:58:31 +0100

    vinio constants: flatten constants out of unecessary arrays

--- app/src/constants/projects/vinio.js
+++ app/src/constants/projects/vinio.js
@@ -1,76 +1,73 @@
-const hero = [
-  {
-    image: "",
-    title: "Vinio",
-    description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes:",
-    technologies: [
-      {
-        name: "MySQL",
-        icon: "mysql",
-      },
-      {
-        name: "SQL",
-        icon: "sql",
-      },
-      {
-        name: "NodeJS",
-        icon: "nodejs",
-      },
-      {
-        name: "ExpressJS",
-        icon: "expressjs",
-      },
-      {
-        name: "JavaScript",
-        icon: "javascript",
-      },
-      {
-        name: "TypeScript",
-        icon: "typescript",
-      },
-      {
-        name: "React",
-        icon: "react",
-      },
-      {
-        name: "Tailwind CSS",
-        icon: "tailwindcss",
-      },
-      {
-        name: "NextJS",
-        icon: "nextjs",
-      },
-      {
-        name: "Vercel",
-        icon: "vercel",
-      },
-    ],
-  }
-];
+const hero =
+{
+  image: "",
+  title: "Vinio",
+  description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes:",
+  technologies: [
+    {
+      name: "MySQL",
+      icon: "mysql",
+    },
+    {
+      name: "SQL",
+      icon: "sql",
+    },
+    {
+      name: "NodeJS",
+      icon: "nodejs",
+    },
+    {
+      name: "ExpressJS",
+      icon: "expressjs",
+    },
+    {
+      name: "JavaScript",
+      icon: "javascript",
+    },
+    {
+      name: "TypeScript",
+      icon: "typescript",
+    },
+    {
+      name: "React",
+      icon: "react",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: "tailwindcss",
+    },
+    {
+      name: "NextJS",
+      icon: "nextjs",
+    },
+    {
+      name: "Vercel",
+      icon: "vercel",
+    },
+  ],
+};
 
-const faq = [
-  {
-    title: "My thoughts & process",
-    thoughts: [
-      {
-        question: "Why did I start this project?",
-        answer: "[placeholder]",
-      },
-      {
-        question: "What do I want to achieve?",
-        answer: "[placeholder]",
-      },
-      {
-        question: "What are the main difficulties I faced?",
-        answer: "[placeholder]",
-      },
-      {
-        question: "What did I learn?",
-        answer: "[placeholder]",
-      },
-    ]
-  }
-];
+const faq = {
+  title: "My thoughts & process",
+  thoughts: [
+    {
+      question: "Why did I start this project?",
+      answer: "[placeholder]",
+    },
+    {
+      question: "What do I want to achieve?",
+      answer: "[placeholder]",
+    },
+    {
+      question: "What are the main difficulties I faced?",
+      answer: "[placeholder]",
+    },
+    {
+      question: "What did I learn?",
+      answer: "[placeholder]",
+    },
+  ]
+};
 
 const explanations = [
   {
@@ -90,13 +87,11 @@ const explanations = [
   },
 ];
 
-const cta = [
-  {
-    title: "Visit the web application",
-    description: "[description]",
-    repository: "https://github.com/stars/cpaulwk/lists/vinio",
-    deployment: "https://vinio-frontend.vercel.app",
-  }
-];
+const cta = {
+  title: "Visit the web application",
+  description: "[description]",
+  repository: "https://github.com/stars/cpaulwk/lists/vinio",
+  deployment: "https://vinio-frontend.vercel.app",
+};
 
-export { hero, faq, explanations }
+export { hero, faq, explanations, cta }
