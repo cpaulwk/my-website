7776af028a53b1129ec37306b24928d71ce6e33d
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:43:06 +0100

    create svg files for hero

---
+++ public/hero/shapes/index.js
@@ -0,0 +1,10 @@
+import shape1 from "shape1.svg";
+import shape2 from "shape2.svg";
+import shape3 from "shape3.svg";
+import shape4 from "shape4.svg";
+import shape5 from "shape5.svg";
+import shape6 from "shape6.svg";
+import shape7 from "shape7.svg";
+import shape8 from "shape8.svg";
+
+export { shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8 }
