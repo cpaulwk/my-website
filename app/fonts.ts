5fbc83556d30b2a690bcf16724493f5eb12e057d
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:42:37 +0100

    create fonts file

---
+++ app/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Dela_Gothic_One, Figtree } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin']
+})
+
+export const dela = Dela_Gothic_One({
+  subsets: ['latin']
+})
+
+export const figtree = Figtree({
+  subsets: ['latin']
+})
+
