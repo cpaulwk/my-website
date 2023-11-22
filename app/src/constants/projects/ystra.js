faed54529f4e3e838e903302e48570b35591c5ee
Paul Chow <paulchow@pauls-mbp.home>
2023-11-22 14:58:56 +0100

    ystra constants: flatten constants out of unecessary arrays

--- app/src/constants/projects/ystra.js
+++ app/src/constants/projects/ystra.js
@@ -1,64 +1,60 @@
-const hero = [
-  {
-    image: "",
-    title: "Ystra",
-    description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes",
-    technologies: [
-      {
-        name: "MongoDB",
-        icon: "mongodb",
-      },
-      {
-        name: "NodeJS",
-        icon: "nodejs",
-      },
-      {
-        name: "ExpressJS",
-        icon: "expressjs",
-      },
-      {
-        name: "JavaScript",
-        icon: "javascript",
-      },
-      {
-        name: "React Native",
-        icon: "reactnative",
-      },
-      {
-        name: "Expo Go",
-        icon: "expogo",
-      },
-      {
-        name: "Redux",
-        icon: "redux",
-      },
-    ],
-  }
-];
+const hero = {
+  image: "",
+  title: "Ystra",
+  description: "Web application aiming to simplify the pairing of food and wines to enhance the choice of wine or dishes",
+  technologies: [
+    {
+      name: "MongoDB",
+      icon: "mongodb",
+    },
+    {
+      name: "NodeJS",
+      icon: "nodejs",
+    },
+    {
+      name: "ExpressJS",
+      icon: "expressjs",
+    },
+    {
+      name: "JavaScript",
+      icon: "javascript",
+    },
+    {
+      name: "React Native",
+      icon: "reactnative",
+    },
+    {
+      name: "Expo Go",
+      icon: "expogo",
+    },
+    {
+      name: "Redux",
+      icon: "redux",
+    },
+  ],
+};
 
-const faq = [
-  {
-    title: "My thoughts & process",
-    thoughts: [
-      {
-        question: "Why did I start this project?",
-        answer: "[answer]",
-      },
-      {
-        question: "What do I want to achieve?",
-        answer: "[answer]",
-      },
-      {
-        question: "What are the main difficulties I faced?",
-        answer: "[answer]",
-      },
-      {
-        question: "What did I learn?",
-        answer: "[answer]",
-      },
-    ]
-  }
-];
+const faq = {
+  title: "My thoughts & process",
+  thoughts: [
+    {
+      question: "Why did I start this project?",
+      answer: "[answer]",
+    },
+    {
+      question: "What do I want to achieve?",
+      answer: "[answer]",
+    },
+    {
+      question: "What are the main difficulties I faced?",
+      answer: "[answer]",
+    },
+    {
+      question: "What did I learn?",
+      answer: "[answer]",
+    },
+  ]
+};
 
 const explanations = [
   {
@@ -78,13 +74,11 @@ const explanations = [
   },
 ];
 
-const cta = [
-  {
-    title: "Visit the web application",
-    description: "[description]",
-    repository: "https://github.com/stars/cpaulwk/lists/ystra",
-    deployment: "",
-  }
-];
+const cta = {
+  title: "Visit the web application",
+  description: "[description]",
+  repository: "https://github.com/stars/cpaulwk/lists/ystra",
+  deployment: "",
+};
 
 export { hero, faq, explanations, cta }
