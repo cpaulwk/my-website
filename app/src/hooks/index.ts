0f467921a7b6db685bee43a0fd417ba212f2f536
Paul Chow <paulchow@pauls-mbp.home>
2023-12-01 15:37:10 +0100

    chore(hooks): create index.ts for centralized hook exports

---
+++ app/src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useDivWidth from "./useDivWidth";
+import useShapeSettings from "./useShapeSettings";
+import useViewportSize from "./useViewportSize";
+
+export { useDivWidth, useShapeSettings, useViewportSize }
