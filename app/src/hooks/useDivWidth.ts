e38d734d7c060e182b150239dcb2d8b3e84b257f
Paul Chow <paulchow@pauls-mbp.home>
2023-12-01 15:32:27 +0100

    feat(hooks): create and implement useDivWidth custom hook

---
+++ app/src/hooks/useDivWidth.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useDivWidth = (divId: string): number => {
+  const [divWidth, setDivWidth] = useState<number>(0);
+
+  const updateDivWidth = (): void => {
+    const yourDiv = document.getElementById(divId);
+
+    if (yourDiv) {
+      const newDivWidth: number = yourDiv.clientWidth;
+      setDivWidth(newDivWidth);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateDivWidth);
+
+    updateDivWidth();
+
+    return () => {
+      window.removeEventListener("resize", updateDivWidth);
+    };
+  }, [divId])
+
+  return divWidth;
+};
+
+export default useDivWidth;
