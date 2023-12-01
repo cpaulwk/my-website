408401d2ec003744fcc62de85a39a6443dbe11a8
Paul Chow <paulchow@pauls-mbp.home>
2023-12-01 15:21:21 +0100

    feat(images): add SVG image files

--- public/hero/shapes/index.js
+++ public/hero/shapes/index.js
@@ -1,10 +1,12 @@
-import shape1 from "shape1.svg";
-import shape2 from "shape2.svg";
-import shape3 from "shape3.svg";
-import shape4 from "shape4.svg";
-import shape5 from "shape5.svg";
-import shape6 from "shape6.svg";
-import shape7 from "shape7.svg";
-import shape8 from "shape8.svg";
+import shape1 from "./shape1.svg";
+import shape2 from "./shape2.svg";
+import shape3 from "./shape3.svg";
+import shape4 from "./shape4.svg";
+import shape5 from "./shape5.svg";
+import shape6 from "./shape6.svg";
+import shape7 from "./shape7.svg";
+import shape8 from "./shape8.svg";
+import shape9 from "./shape9.svg";
+import shape10 from "./shape10.svg";
 
-export { shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8 }
+export { shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape8, shape9, shape10 }
