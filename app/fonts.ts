7579c03c6afde0314f5d9ec604c614e5e92b1dff
Paul Chow <paulchow@pauls-mbp.home>
2023-12-01 15:26:17 +0100

    feat(fonts): add new fonts to the project

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,14 +1,18 @@
 import { Inter, Dela_Gothic_One, Figtree } from 'next/font/google'
 
 export const inter = Inter({
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: '--font-inter'
 })
 
 export const dela = Dela_Gothic_One({
-  subsets: ['latin']
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-dela-gothic-one'
 })
 
 export const figtree = Figtree({
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: '--font-figtree'
 })
 
