3b1b7645954a08b00d870e0b1a833c2ff31a0596
Paul Chow <paulchow@pauls-mbp.home>
2023-12-01 15:28:29 +0100

    feat(tailwind): add new classes to tailwindcss.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,16 +7,34 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      '3xs': '280px',
+      '2xs': '400px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1600px',
+    },
+    colors: {
+      'red': '#ff0000',
+      'white': '#ffffff',
+      'black': '#000000',
+      'theme-dark-blue': '#0a1128',
+      'theme-light-blue': '#3d5a80',
+      'theme-green': '#a5be00',
+    },
+    fontFamily: {
+      dela: ['var(--font-dela-gothic-one)'],
+      figtree: ['var(--font-figtree)'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily: {
-        dela: ['var(--font-dela-gothic-one)'],
-        figtree: ['var(--font-figtree)'],
-      }
     },
   },
   plugins: [],
