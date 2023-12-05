f8768420343dc69c734c58af4bb9cf45772b916e
Paul Chow <paulchow@pauls-mbp.home>
2023-12-05 11:23:03 +0100

    feat: update tailwind.config.js with custom classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,19 +22,30 @@ const config: Config = {
       'white': '#ffffff',
       'black': '#000000',
       'theme-dark-blue': '#0a1128',
-      'theme-light-blue': '#3d5a80',
+      'theme-light-blue': '#05668D',
       'theme-green': '#a5be00',
+      'theme-white': '#ebf2fa',
     },
     fontFamily: {
       dela: ['var(--font-dela-gothic-one)'],
       figtree: ['var(--font-figtree)'],
     },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontSize: {
+        'xs': '0.75rem', //  12px
+        'sm': '0.875rem', // 14px
+        'base': '1rem', // 16px
+        'lg': '1.125rem', // 18px
+        'xl': '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem', // 72px
+        '8xl': '6rem', // 96px
+        '9xl': '8rem', // 128px
+      }
     },
   },
   plugins: [],
