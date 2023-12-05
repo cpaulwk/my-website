7f83f41e08d2cebd79dd8bf9df761aa13c8753cf
Paul Chow <paulchow@pauls-mbp.home>
2023-12-05 11:12:50 +0100

    chore(components): centralize component exports in index file

---
+++ app/src/components/index.ts
@@ -0,0 +1,8 @@
+import Hero from "./pages/Hero"
+import SectionWrapper from "./atoms/SectionWrapper"
+import Introduction from "./organisms/Introduction"
+import Skills from "./organisms/Skills"
+import ViewportDisplay from "./debugger/ViewportDisplay"
+
+
+export { Hero, SectionWrapper, Introduction, ViewportDisplay, Skills }
