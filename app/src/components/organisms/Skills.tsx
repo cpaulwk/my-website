9629e94b500f6adc4fe56e13e58723f70f95a65e
Paul Chow <paulchow@pauls-mbp.home>
2023-12-05 11:10:38 +0100

    feat(components): add Skills component

---
+++ app/src/components/organisms/Skills.tsx
@@ -0,0 +1,15 @@
+import { SectionWrapper } from ".."
+
+const Skills = () => {
+  return (
+    <section className="flex justify-center items-center bg-theme-light-blue max-w-screen">
+      <SectionWrapper>
+        <div className="flex flex-col h-full w-full">
+          <h1 className="white"></h1>
+        </div>
+      </ SectionWrapper>
+    </section>
+  )
+}
+
+export default Skills
