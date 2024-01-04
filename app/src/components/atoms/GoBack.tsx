f69d4dfafba54a56e63aa6f2a2d8ca47393b58fe
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 20:50:24 +0100

    feat(components): add GoBack button component

---
+++ app/src/components/atoms/GoBack.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+const GoBack = () => {
+  return (
+    <Link href="/" className="absolute top-0 left-0 bg-white text-black">GoBack</Link>
+  )
+}
+
+export default GoBack
