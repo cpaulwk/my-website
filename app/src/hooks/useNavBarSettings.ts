82d4dbf4386c1ed072c0282641f4685a2fa964b9
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 23:02:39 +0100

    chore(hooks): add Typescript interfaces to useNavBarSettings hook

--- app/src/hooks/useNavBarSettings.ts
+++ app/src/hooks/useNavBarSettings.ts
@@ -1,7 +1,24 @@
 "use client"
 import { useEffect, useState } from 'react'
+import { IconType } from 'react-icons';
 
-const useNavBarSettings = ({ navNavigation, navigation }) => {
+interface NavNavigationProps {
+  navNavigation: {
+    projects: string[];
+    icons: {
+      name: string;
+      icon: IconType;
+    }[];
+  }
+}
+
+interface NavigationProps {
+  navigation: {
+    page: string,
+  }
+}
+
+const useNavBarSettings = ({ navNavigation, navigation }: NavNavigationProps & NavigationProps) => {
   const [nextProject, setNextProject] = useState<null | String>();
   const [previousProject, setPreviousProject] = useState<null | String>();
   const [currentProject, setCurrentProject] = useState<String>();
