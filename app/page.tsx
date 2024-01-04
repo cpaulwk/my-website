bcf8424383ccfec2ecf6f3723103f1d2dc81e764
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:15:58 +0100

    chore: update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { Hero, Introduction } from "@/app/src/components";
-import Skills from "./src/components/organisms/Skills";
+import { Contact, HeroHome, Introduction, Projects, Skills, ViewportDisplay } from "@/app/src/components";
 
 const Home = () => {
 
   return (
     <main className="flex flex-col min-h-screen max-w-screen overflow-hidden">
-      <Hero />
+      <HeroHome />
       <Introduction />
       <Skills />
+      <Projects />
+      <Contact />
     </main>
   )
 }
