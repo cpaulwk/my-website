86a73c046979f2b2f98c70fe06e2395784e22478
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:10:09 +0100

    chore(hooks): add hook exports

--- app/src/hooks/index.ts
+++ app/src/hooks/index.ts
@@ -1,5 +1,7 @@
 import useDivWidth from "./useDivWidth";
 import useShapeSettings from "./useShapeSettings";
+import useSkillsSettings from "./useSkillsSettings";
 import useViewportSize from "./useViewportSize";
+import useNavBarSettings from "./useNavBarSettings"
 
-export { useDivWidth, useShapeSettings, useViewportSize }
+export { useDivWidth, useShapeSettings, useSkillsSettings, useViewportSize, useNavBarSettings }
