cf96a4b64d9f2fee7d51664a28dbb760a222d372
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:04:16 +0100

    feat(components): add QA component

---
+++ app/src/components/organisms/Projects/QA.tsx
@@ -0,0 +1,45 @@
+import { SectionWrapper } from "@/app/src/components"
+import Image from "next/image"
+
+const QA = ({ qa }) => {
+  return (
+    <section className="bg-theme-white max-w-screen flex flex-col items-center justify-center">
+      <SectionWrapper>
+        <div className="flex flex-col h-full w-full items-center gap-8">
+          <h1 className="text-theme-light-blue">{qa.title}</h1>
+          <div className="flex flex-wrap w-full justify-between items-center">
+            {qa.thoughts.map((info, i) => {
+              return (
+                <div key={i} className={i % 2 === 0 ? `flex md:flex-row flex-col w-full min-h-[365px] bg-theme-white` : `flex md:flex-row-reverse flex-col w-full min-h-[365px] bg-white`}>
+                  <div className="flex flex-col basis-1/2 p-6 gap-y-4">
+                    <h2 className={i % 2 === 0 ? `text-theme-light-blue` : `text-black`}>{info.question}</h2>
+                    <div className="flex flex-col gap-6">
+                      {info.answer.map((text, i) => {
+                        return (
+                          <p key={i} className="text-justify">{text}</p>
+                        )
+                      })}
+                    </div>
+                  </div>
+                  <div className="flex basis-1/2 justify-center items-center">
+                    <div className="flex flex-wrap justify-center items-center max-w-[365px] max-h-[365px] max-md:p-6">
+                      {info.images.map((icon, i) => {
+                        return (
+                          <div key={i} className="flex basis-1/3">
+                            <Image src={icon.source} style={{ objectFit: "contain" }} alt={icon.name} />
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </SectionWrapper>
+    </section>
+  )
+}
+
+export default QA
