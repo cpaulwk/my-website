08e83a644f99246227fd2852471bdc8a61c68f5c
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:08:49 +0100

    chore(constant): add navigation file

---
+++ app/src/constants/projects/navigation.ts
@@ -0,0 +1,29 @@
+import { FaHouse } from "react-icons/fa6"
+
+import {
+  FaChevronLeft,
+  FaChevronRight,
+} from "react-icons/fa"
+
+const navigation = {
+  projects: [
+    "ystra",
+    "vinio",
+  ],
+  icons: [
+    {
+      name: "House",
+      icon: FaHouse,
+    },
+    {
+      name: "Previous",
+      icon: FaChevronLeft,
+    },
+    {
+      name: "Next",
+      icon: FaChevronRight,
+    },
+  ]
+}
+
+export { navigation }
