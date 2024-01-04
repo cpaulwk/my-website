38889789405c7e378e9dceb3d130b8f75a187c77
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:07:14 +0100

    chore(components): add new component exports

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,8 +1,34 @@
-import Hero from "./pages/Hero"
+import HeroHome from "./organisms/Home/Hero"
+import Introduction from "./organisms/Home/Introduction"
+import Projects from "./organisms/Home/Projects"
 import SectionWrapper from "./atoms/SectionWrapper"
-import Introduction from "./organisms/Introduction"
-import Skills from "./organisms/Skills"
+import Skills from "./organisms/Home/Skills"
 import ViewportDisplay from "./debugger/ViewportDisplay"
+import GoBack from "./atoms/GoBack"
+import HeroProjects from "./organisms/Projects/Hero"
+import DivWidth from "./debugger/DivWidth"
+import ProjectPage from "./pages/ProjectPage"
+import Contact from "./organisms/Home/Contact"
+import NavBar from "./organisms/Projects/NavBar"
+import QA from "./organisms/Projects/QA"
+import Preview from "./organisms/Projects/Preview"
+import Cta from "./organisms/Projects/Cta"
 
 
-export { Hero, SectionWrapper, Introduction, ViewportDisplay, Skills }
+export {
+  HeroHome,
+  HeroProjects,
+  Introduction,
+  Skills,
+  Projects,
+  Contact,
+  SectionWrapper,
+  GoBack,
+  ViewportDisplay,
+  DivWidth,
+  ProjectPage,
+  NavBar,
+  QA,
+  Preview,
+  Cta
+}
