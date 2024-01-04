b04fba93e3e81d3487a83dddc84f1a08ddcd057c
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:14:41 +0100

    feat(utils): add pickRandomShapes utils

---
+++ app/src/utils/pickRandomShapes.ts
@@ -0,0 +1,17 @@
+const pickRandomShapes = (array) => {
+  if (array.length <= 9) {
+    return;
+  }
+
+  const copyArray = [...array];
+
+  // Shuffle the array using Fisher-Yates algorithm
+  for (let i = copyArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
+  }
+
+  return copyArray.slice(0, 9);
+}
+
+export default pickRandomShapes
