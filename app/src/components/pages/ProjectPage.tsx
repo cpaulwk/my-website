2544d5c67985d2150c80d3f494b802f774c958a5
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 22:41:42 +0100

    chore(components): add Typescript interfaces to ProjectPage component

--- app/src/components/pages/ProjectPage.tsx
+++ app/src/components/pages/ProjectPage.tsx
@@ -1,6 +1,70 @@
-import { Preview, GoBack, HeroProjects, QA, Cta, NavBar } from "@/app/src/components"
+import { Preview, HeroProjects, QA, Cta, NavBar } from "@/app/src/components"
+import { StaticImageData } from "next/image";
+import { IconType } from 'react-icons';
 
-const ProjectPage = ({ navigation, hero, qa, preview, cta }) => {
+interface NavigationProps {
+  navigation: {
+    page: string,
+  }
+}
+
+interface Technology {
+  name: string;
+  icon: IconType;
+}
+
+interface HeroProps {
+  hero: {
+    image: StaticImageData;
+    title: string;
+    description: string;
+    technologies: Technology[];
+  }
+}
+
+interface PictureQA {
+  name: string;
+  source: string;
+}
+
+interface Thought {
+  question: string;
+  answer: string[];
+  images: PictureQA[];
+}
+
+interface QAProps {
+  qa: {
+    title: string;
+    thoughts: Thought[];
+  }
+}
+
+interface PicturePreview {
+  name: string;
+  source: StaticImageData;
+}
+
+interface PreviewProps {
+  preview: {
+    title: string;
+    images: PicturePreview[];
+  }
+}
+
+interface Button {
+  name: string;
+  url: string;
+}
+
+interface CtaProps {
+  cta: {
+    title: string;
+    buttons: Button[];
+  }
+}
+
+const ProjectPage = ({ navigation, hero, qa, preview, cta }: NavigationProps & HeroProps & QAProps & PreviewProps & CtaProps) => {
 
   return (
     <>
