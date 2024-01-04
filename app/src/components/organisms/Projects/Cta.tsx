be64c3a2406d4e776b9cbdc15ed8227b019bac80
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:03:20 +0100

    feat(components): add Cta component

---
+++ app/src/components/organisms/Projects/Cta.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { SectionWrapper } from "@/app/src/components"
+
+const Cta = ({ cta }) => {
+  const openExternalLink = (url: string) => {
+    window.open(url, '_blank');
+  }
+
+  return (
+    <section className="bg-theme-light-blue text-theme-white max-w-screen flex flex-col items-center justify-center">
+      <SectionWrapper>
+        <div className="flex flex-col h-full w-full items-center gap-8">
+          <h1 className="text-center">{cta.title}</h1>
+          <div className="flex gap-6">
+            {cta.buttons.map((btn, i) => {
+              return (
+                <button key={i} onClick={() => openExternalLink(btn.url)} className={i % 2 === 0 ? `border border-black bg-black text-white rounded-lg py-2 px-4` : `border border-theme-green bg-theme-green text-black rounded-lg py-2 px-4`}>
+                  {btn.name}
+                </button>
+              )
+            })}
+          </div>
+        </div>
+      </SectionWrapper>
+
+    </section>
+  )
+}
+
+export default Cta
