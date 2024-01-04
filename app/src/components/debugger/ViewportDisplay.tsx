d585e936cf253f9a8d236c7d5094787321878306
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 20:56:02 +0100

    feat(debugger): update ViewportDisplay component (non-production)

--- app/src/components/debugger/ViewportDisplay.tsx
+++ app/src/components/debugger/ViewportDisplay.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from "react";
-import useViewportSize from "@/app/src/hooks/useViewportSize"
+import { useViewportSize } from "@/app/src/hooks"
 
 export default function ViewportDisplay() {
   const viewportSize = useViewportSize();
 
   return (
-    <div className="absolute top-0 text-red">
-      <p>Viewport Width: {viewportSize.width}px</p>
-      <p>Viewport Height: {viewportSize.height}px</p>
+    <div className="fixed z-10 top-0 text-red w-full flex justify-center">
+      <div className="bg-white">
+        <p>Viewport Width: {viewportSize.width}px</p>
+        <p>Viewport Height: {viewportSize.height}px</p>
+      </div>
     </div>
   )
 }
