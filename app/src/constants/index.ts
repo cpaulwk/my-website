6eb9ce77f4e8f3b9b3e082327619902e86fc3e46
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:09:47 +0100

    chore(constant): add constant exports

---
+++ app/src/constants/index.ts
@@ -0,0 +1,29 @@
+import {
+  hero as homeHero,
+  introduction,
+  skills,
+  projects,
+  socials
+} from "./home/home"
+
+import {
+  navigation as vinioNavigation,
+  hero as vinioHero,
+  qa as vinioQa,
+  preview as vinioPreview,
+  cta as vinioCta
+} from "./projects/vinio"
+
+import {
+  navigation as ystraNavigation,
+  hero as ystraHero,
+  qa as ystraQa,
+  preview as ystraPreview,
+  cta as ystraCta
+} from "./projects/ystra"
+
+import {
+  navigation as navNavigation,
+} from "./projects/navigation"
+
+export { homeHero, introduction, skills, projects, socials, vinioNavigation, vinioHero, vinioQa, vinioPreview, vinioCta, ystraNavigation, ystraHero, ystraQa, ystraPreview, ystraCta, navNavigation }
