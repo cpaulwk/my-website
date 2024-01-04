4465bab9afaaa9b4be6bcc7b0176bf90b6c3d58f
Paul Chow <paulchow@pauls-mbp.home>
2024-01-04 21:18:56 +0100

    chore(images): add images

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,3 +1,26 @@
-import portrait from "./portrait.png";
+import portrait from "./portrait.png"
+import vinio from "./vinio.png"
+import ystra from "./ystra.png"
+import kid_in_wine_aisle from "./projects/vinio/kid_in_wine_aisle.png"
+import mountain_of_grapes from "./projects/vinio/mountain_of_grapes.png"
+import phone_holography from "./projects/vinio/phone_holography.png"
+import teen_in_library from "./projects/vinio/teen_in_library.png"
+import vinio_pairing_mode from "./projects/vinio/vinio_pairing_mode.png"
+import vinio_suggestion_mode from "./projects/vinio/vinio_suggestion_mode.png"
+import ystra_generated_results_page from "./projects/ystra/ystra_generated_results_page.png"
+import ystra_cart_page from "./projects/ystra/ystra_cart_page.png"
 
-export { portrait }
+
+export {
+  portrait,
+  vinio,
+  ystra,
+  kid_in_wine_aisle,
+  mountain_of_grapes,
+  phone_holography,
+  teen_in_library,
+  vinio_pairing_mode,
+  vinio_suggestion_mode,
+  ystra_generated_results_page,
+  ystra_cart_page,
+}
