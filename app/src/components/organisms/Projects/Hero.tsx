f10c6d89277e5e103bca703bf102f56e223bb3c1
Paul Chow <paulchow@pauls-mbp.home>
2024-01-05 22:56:19 +0100

    chore(components): update Hero project pages component style

--- app/src/components/organisms/Projects/Hero.tsx
+++ app/src/components/organisms/Projects/Hero.tsx
@@ -27,12 +27,12 @@ const Hero = ({ hero }: HeroProps) => {
 
         <div className="flex flex-col gap-y-2">
           <h1 className="text-theme-white w-full">{hero.title}</h1>
-          <div className="flex gap-x-6">
+          <div className="flex flex-col md:flex-row gap-6">
             <p className="text-theme-white text-2xl w-full text-justify">{hero.description}</p>
             <div className="flex flex-wrap gap-y-6">
               {hero.technologies.map((tech, i) => {
                 return (
-                  <div key={i} className="flex flex-col min-w-[95px] justify-center items-center skills4P:basis-1/4 sm:basis-1/3 xs:basis-1/2 basis-full max-xs:grow">
+                  <div key={i} className="flex flex-col min-w-[95px] justify-center items-center skills4P:basis-1/4 skills4:basis-1/3 2xs:basis-1/2 basis-full">
                     {tech.icon({ color: '#ebf2fa', fontSize: '3em' })}
                     <p className="text-theme-white">{tech.name}</p>
                   </div>
