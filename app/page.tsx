83f1a4f823076cb5944c3e11259759a12c9d4d38
Paul Chow <paulchow@pauls-mbp.home>
2024-01-12 08:58:47 +0100

    chore(page): cleanup code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Contact, HeroHome, Introduction, Projects, Skills } from "@/app/src/components";
 
 const Home = () => {
-
   return (
     <main className="flex flex-col min-h-screen max-w-screen overflow-hidden">
       <HeroHome />
